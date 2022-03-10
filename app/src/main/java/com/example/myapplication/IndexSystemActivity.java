dfa6ae6352e2eaf41f6eed163902a726cff6c8e3
zsj <1013361303@qq.com>
2022-03-10 22:08:10 +0800

    登录 出现错误 见mainact中的login方法

---
+++ app/src/main/java/com/example/myapplication/IndexSystemActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IndexSystemActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index_system);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sp = getSharedPreferences( "config" , MODE_PRIVATE );
+        if (sp.getBoolean("is_user_first_join",true)){
+            startActivity(new Intent(MainActivity.this, WelcomeActivity.class)); //跳转到LoginActivity类中
+        }
 
+
+    }
+
+    /**
+     * todo   获取同意框然后对登录进行监听
+     * @param view
+     */
+    public void login(View view) {
+        //获取的是同意
+        CheckBox checkBox= findViewById(R.id.read_agree);
+        boolean checked = checkBox.isChecked();
+        if (checked){
+            setContentView(R.layout.index_system);
+        }else {
+            System.out.println("为选中");
+        }
     }
 }

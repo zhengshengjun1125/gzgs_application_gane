4d6e556dbf6961858eacecd61040a04503a889fe
zsj <1013361303@qq.com>
2022-03-10 23:35:22 +0800

    解决登录错误

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,19 +13,25 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sp = getSharedPreferences( "config" , MODE_PRIVATE );
-        if (sp.getBoolean("is_user_first_join",true)){
+        btn = findViewById(R.id.btn_agree);
+
+        //暂时创建逻辑库
+        SharedPreferences sp = getSharedPreferences("config", MODE_PRIVATE);
+        if (sp.getBoolean("is_user_first_join", false)) {
             startActivity(new Intent(MainActivity.this, WelcomeActivity.class)); //跳转到LoginActivity类中
         }
 
@@ -33,17 +39,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * todo   获取同意框然后对登录进行监听
      * @param view
      */
     public void login(View view) {
-        //获取的是同意
-        CheckBox checkBox= findViewById(R.id.read_agree);
-        boolean checked = checkBox.isChecked();
-        if (checked){
-            setContentView(R.layout.index_system);
+        //获取到同意框
+        CheckBox read_agree = findViewById(R.id.read_agree);
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.user_pass);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        if (username.getText().toString() != "admin" &&
+                password.getText().toString() != "admin"
+                && read_agree.isChecked() == false) {
+            dialog.setTitle("提示").setMessage("账号或者密码错误,也有可能是您没有同意用户协议").setPositiveButton("知道了", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            }).show();
+            //Toast.makeText(this,"账号或者密码错误,当前为测试账号",Toast.LENGTH_SHORT).show();
         }else {
-            System.out.println("为选中");
+            //没有错了，直接进入页面即可
+            setContentView(R.layout.index_system);
         }
+
     }
+
+/*
+ else {
+            System.out.println(checkBox.isChecked());
+            //没有被选中
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new AlertDialog.Builder(MainActivity.this).setTitle("信息提示")//设置对话框标题
+
+                            .setMessage("是否同意并进入系统？")
+                            .setPositiveButton("是", new DialogInterface.OnClickListener() {//添加确定按钮
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {//确定按钮的响应事件
+                                    checkBox.setChecked(checkBox.isChecked());
+                                    login(view);
+                                }
+                            }).setNegativeButton("否", new DialogInterface.OnClickListener() {//添加返回按钮
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {//响应事件
+
+                        }
+
+                    }).show();//在按键响应事件中显示此对话框
+                }
+            });
+        }
+ */
 }
